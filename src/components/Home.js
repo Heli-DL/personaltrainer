97865a9ffd1a8f7b30e98d9f58e9ba3c55245afd
Heli-DL <heli.delorme@outlook.com>
2022-12-11 19:11:08 +0200

    second commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,18 +12,18 @@ export default function Home() {
         <h1 style={{fontSize: '5em', margin: '4rem auto 3rem 6rem'}}>Personal trainer database</h1>
       </Box>
       <Button sx={{marginLeft: '6rem', marginRight: 4}} color="primary" variant="contained" size="large">
-        <Link style={{textDecoration: "none", color: "white"}} to={'./Customers'}>CUSTOMERS</Link>
+        <Link style={{textDecoration: "none", color: "white"}} to={'/Customers'}>CUSTOMERS</Link>
       </Button>
       <Button sx={{marginRight: 2.5}} color="primary" variant="contained" size="large">
-        <Link style={{textDecoration: "none", color: "white"}} to={'./Trainings'}>TRAININGS</Link>
+        <Link style={{textDecoration: "none", color: "white"}} to={'/Trainings'}>TRAININGS</Link>
       </Button>
       <Button sx={{ padding: 0 }}>
-        <Link style={{textDecoration: "none", color: "white"}} to={'./Calendar'}>
+        <Link style={{textDecoration: "none", color: "white"}} to={'/Calendar'}>
           <CalendarMonthIcon color="primary" sx={{ fontSize: 50 }}/>
         </Link>
       </Button>
       <Button sx={{ padding: 0 }}>
-        <Link style={{textDecoration: "none", color: "white"}} to={'./Charts'}>
+        <Link style={{textDecoration: "none", color: "white"}} to={'/Charts'}>
           <LeaderboardIcon color="primary" sx={{ fontSize: 50 }}/>
         </Link>
       </Button>
